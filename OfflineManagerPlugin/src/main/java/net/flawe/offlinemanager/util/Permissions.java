96835006a435d1357be75db8e4b607ecdb09e073
flaweoff <67743907+flaweoff@users.noreply.github.com>
2021-01-02 19:30:50 +0300

    Constants position change

--- OfflineManagerPlugin/src/main/java/net/flawe/offlinemanager/util/Permissions.java
+++ OfflineManagerPlugin/src/main/java/net/flawe/offlinemanager/util/Permissions.java
@@ -3,17 +3,17 @@ package net.flawe.offlinemanager.util;
 public class Permissions {
     public static final String invsee = "offlinemanager.invsee";
     public static final String invsee_armor = "offlinemanager.invsee.armor";
+    public static final String enderchest = "offlinemanager.enderchest";
     public static final String reload = "offlinemanager.reload";
     public static final String updateConfig = "offlinemanager.update.config";
     public static final String teleport = "offlinemanager.teleport";
+    public static final String tphere = "offlinemanager.tphere";
+    public static final String adventure = "offlinemanager.adventure";
     public static final String creative = "offlinemanager.creative";
     public static final String survival = "offlinemanager.survival";
     public static final String spectator = "offlinemanager.spectator";
-    public static final String adventure = "offlinemanager.adventure";
-    public static final String tphere = "offlinemanager.tphere";
     public static final String clear = "offlinemanager.clear";
     public static final String kill = "offlinemanager.kill";
     public static final String heal = "offlinemanager.heal";
     public static final String feed = "offlinemanager.feed";
-    public static final String enderchest = "offlinemanager.enderchest";
 }
