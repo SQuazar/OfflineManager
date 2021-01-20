4400cde5a449b9008e567ef9a9009da226a4cd12
flaweoff <67743907+flaweoff@users.noreply.github.com>
2021-01-20 03:45:21 +0300

    Update files

--- OfflineManagerAPI/src/main/java/net/flawe/offlinemanager/api/IArmorInventory.java
+++ OfflineManagerAPI/src/main/java/net/flawe/offlinemanager/api/IArmorInventory.java
@@ -2,6 +2,12 @@ package net.flawe.offlinemanager.api;
 
 import org.bukkit.inventory.Inventory;
 
+/**
+ * The interface Armor inventory.
+ */
 public interface IArmorInventory {
+    /**
+     * @return Armor inventory
+     */
     Inventory getInventory();
 }
