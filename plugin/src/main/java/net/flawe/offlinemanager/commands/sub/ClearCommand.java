fe44cc06dc00b27ef25fbb8cd1fe939a12c73d37
flaweoff <67743907+flaweoff@users.noreply.github.com>
2021-05-16 02:32:00 +0300

    Changed directory and update api usage

--- plugin/src/main/java/net/flawe/offlinemanager/commands/sub/ClearCommand.java
+++ plugin/src/main/java/net/flawe/offlinemanager/commands/sub/ClearCommand.java
@@ -1,14 +1,12 @@
-package net.flawe.offlinemanager.commands.subs;
+package net.flawe.offlinemanager.commands.sub;
 
 import net.flawe.offlinemanager.api.IUser;
 import net.flawe.offlinemanager.api.enums.SavePlayerType;
 import net.flawe.offlinemanager.commands.OMCommand;
-import net.flawe.offlinemanager.events.ClearOfflineInventoryEvent;
+import net.flawe.offlinemanager.api.events.inventory.ClearOfflineInventoryEvent;
 import net.flawe.offlinemanager.util.configuration.PlaceholderUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-
-import static net.flawe.offlinemanager.util.constants.Messages.*;
 
 public class ClearCommand extends OMCommand {
 
@@ -22,13 +20,13 @@ public class ClearCommand extends OMCommand {
     public void execute(Player player, String[] args) {
         addPlaceholder("%player%", player.getName());
         String msg;
-        if (!api.getConfigManager().getCommandClearInventoryConfig().enabled()) {
-            msg = api.getConfigManager().getMessageString(player, functionDisabled);
+        if (!settings.getClearConfiguration().enabled()) {
+            msg = api.getConfigManager().fillMessage(player, messages.getFunctionDisabled());
             player.sendMessage(PlaceholderUtil.fillPlaceholders(msg, getPlaceholders()));
             return;
         }
         if (args.length == 1) {
-            msg = api.getConfigManager().getMessageString(player, enterNickname);
+            msg = api.getConfigManager().fillMessage(player, messages.getEnterNickname());
             player.sendMessage(PlaceholderUtil.fillPlaceholders(msg, getPlaceholders()));
             return;
         }
@@ -36,12 +34,12 @@ public class ClearCommand extends OMCommand {
         addPlaceholder("%target%", playerName);
         Player target = Bukkit.getPlayerExact(playerName);
         if (target != null && target.isOnline()) {
-            msg = api.getConfigManager().getMessageString(player, playerIsOnline);
+            msg = api.getConfigManager().fillMessage(player, messages.getPlayerIsOnline());
             player.sendMessage(PlaceholderUtil.fillPlaceholders(msg, getPlaceholders()));
             return;
         }
         if (!api.getStorage().hasPlayer(playerName)) {
-            msg = api.getConfigManager().getMessageString(player, playerNotFound);
+            msg = api.getConfigManager().fillMessage(player, messages.getPlayerNotFound());
             player.sendMessage(PlaceholderUtil.fillPlaceholders(msg, getPlaceholders()));
             return;
         }
@@ -52,7 +50,7 @@ public class ClearCommand extends OMCommand {
             return;
         user.getPlayer().getInventory().clear();
         user.save(SavePlayerType.INVENTORY);
-        msg = api.getConfigManager().getMessageString(player, clearInventory);
+        msg = api.getConfigManager().fillMessage(player, messages.getClearInventory());
         player.sendMessage(PlaceholderUtil.fillPlaceholders(msg, getPlaceholders()));
     }
 }
