5acf142c6dfd25bd493006281231353a94521482
flaweoff <67743907+flaweoff@users.noreply.github.com>
2021-05-16 02:37:29 +0300

    Added interface for serialize and deserialize configuration

---
+++ api/src/main/java/net/flawe/offlinemanager/api/configuration/Configuration.java
@@ -0,0 +1,9 @@
+package net.flawe.offlinemanager.api.configuration;
+
+/**
+ * Interface that all configuration classes implement to load configuration parameters into a class
+ *
+ * @author flawe
+ */
+public interface Configuration {
+}
