300bab71bd52cb2f5fc28eaffdee96f44e3d9492
flaweoff <67743907+flaweoff@users.noreply.github.com>
2021-05-16 02:53:18 +0300

    Changed directory and update api usage

--- plugin/src/main/java/net/flawe/offlinemanager/commands/sub/InventoryCommand.java
+++ plugin/src/main/java/net/flawe/offlinemanager/commands/sub/InventoryCommand.java
@@ -1,16 +1,16 @@
-package net.flawe.offlinemanager.commands.subs;
+package net.flawe.offlinemanager.commands.sub;
 
 import net.flawe.offlinemanager.api.IUser;
 import net.flawe.offlinemanager.api.enums.ActiveType;
 import net.flawe.offlinemanager.commands.OMCommand;
-import net.flawe.offlinemanager.events.OpenOfflineInventoryEvent;
-import net.flawe.offlinemanager.holders.OfflineArmorInventoryHolder;
-import net.flawe.offlinemanager.holders.OfflineInventoryHolder;
+import net.flawe.offlinemanager.api.events.inventory.OpenOfflineInventoryEvent;
+import net.flawe.offlinemanager.inventory.holders.OfflineArmorInventoryHolder;
+import net.flawe.offlinemanager.inventory.holders.OfflineInventoryHolder;
 import net.flawe.offlinemanager.util.configuration.PlaceholderUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
-import static net.flawe.offlinemanager.util.constants.Messages.*;
+
 import static net.flawe.offlinemanager.util.constants.Permissions.*;
 
 public class InventoryCommand extends OMCommand {
@@ -25,18 +25,18 @@ public class InventoryCommand extends OMCommand {
         addPlaceholder("%permission%", getPermission());
         addPlaceholder("%function%", "Offline Inventory");
         String msg;
-        if (!api.getConfigManager().getInventoryConfig().interact()) {
-            msg = api.getConfigManager().getMessageString(player, functionDisabled);
+        if (!settings.getInventoryConfiguration().interact()) {
+            msg = api.getConfigManager().fillMessage(player, messages.getFunctionDisabled());
             player.sendMessage(PlaceholderUtil.fillPlaceholders(msg, getPlaceholders()));
             return;
         }
         if (!hasPermission(player)) {
-            msg = api.getConfigManager().getMessageString(player, permissionDeny);
+            msg = api.getConfigManager().fillMessage(player, messages.getPermissionDeny());
             player.sendMessage(PlaceholderUtil.fillPlaceholders(msg, getPlaceholders()));
             return;
         }
         if (args.length == 1) {
-            msg = api.getConfigManager().getMessageString(player, enterNickname);
+            msg = api.getConfigManager().fillMessage(player, messages.getEnterNickname());
             player.sendMessage(PlaceholderUtil.fillPlaceholders(msg, getPlaceholders()));
             return;
         }
@@ -44,12 +44,12 @@ public class InventoryCommand extends OMCommand {
         addPlaceholder("%target%", playerName);
         Player t = Bukkit.getPlayerExact(playerName);
         if (t != null && t.isOnline()) {
-            msg = api.getConfigManager().getMessageString(player, playerIsOnline);
+            msg = api.getConfigManager().fillMessage(player, messages.getPlayerIsOnline());
             player.sendMessage(PlaceholderUtil.fillPlaceholders(msg, getPlaceholders()));
             return;
         }
         if (!api.getStorage().hasPlayer(playerName)) {
-            msg = api.getConfigManager().getMessageString(player, playerNotFound);
+            msg = api.getConfigManager().fillMessage(player, messages.getPlayerNotFound());
             player.sendMessage(PlaceholderUtil.fillPlaceholders(msg, getPlaceholders()));
             return;
         }
@@ -57,11 +57,11 @@ public class InventoryCommand extends OMCommand {
         OpenOfflineInventoryEvent event = new OpenOfflineInventoryEvent(player, user);
         if (args.length == 2) {
             if (api.getSession().containsValue(user.getUUID(), ActiveType.INVENTORY)) {
-                msg = api.getConfigManager().getMessageString(player, alreadyBeingEdited);
+                msg = api.getConfigManager().fillMessage(player, messages.getAlreadyBeingEdited());
                 player.sendMessage(PlaceholderUtil.fillPlaceholders(msg, getPlaceholders()));
                 return;
             }
-            OfflineInventoryHolder holder = new OfflineInventoryHolder(user, player, api.getConfigManager().getInventoryConfig().getName());
+            OfflineInventoryHolder holder = new OfflineInventoryHolder(user, player, settings.getInventoryConfiguration().getName());
             event.setInventory(holder.getInventory());
             event.setInventoryType(holder.getInventoryType());
             Bukkit.getPluginManager().callEvent(event);
@@ -72,18 +72,18 @@ public class InventoryCommand extends OMCommand {
             return;
         }
         if (args[2].equalsIgnoreCase("armor")) {
-            if (!player.hasPermission(invsee_armor)) {
-                msg = api.getConfigManager().getMessageString(player, permissionDeny);
-                addPlaceholder("%permission%", invsee_armor);
+            if (!player.hasPermission(OFFLINEMANAGER_INVSEE_ARMOR)) {
+                msg = api.getConfigManager().fillMessage(player, messages.getPermissionDeny());
+                addPlaceholder("%permission%", OFFLINEMANAGER_INVSEE_ARMOR);
                 player.sendMessage(PlaceholderUtil.fillPlaceholders(msg, getPlaceholders()));
                 return;
             }
             if (api.getSession().containsValue(user.getUUID(), ActiveType.ARMOR_INVENTORY)) {
-                msg = api.getConfigManager().getMessageString(player, alreadyBeingEdited);
+                msg = api.getConfigManager().fillMessage(player, messages.getAlreadyBeingEdited());
                 player.sendMessage(PlaceholderUtil.fillPlaceholders(msg, getPlaceholders()));
                 return;
             }
-            OfflineArmorInventoryHolder armorInventoryHolder = new OfflineArmorInventoryHolder(user, player, api.getConfigManager().getArmorInventoryConfig().getName());
+            OfflineArmorInventoryHolder armorInventoryHolder = new OfflineArmorInventoryHolder(user, player, settings.getArmorInventoryConfiguration().getName());
             event.setInventory(armorInventoryHolder.getInventory());
             event.setInventoryType(armorInventoryHolder.getInventoryType());
             Bukkit.getPluginManager().callEvent(event);
