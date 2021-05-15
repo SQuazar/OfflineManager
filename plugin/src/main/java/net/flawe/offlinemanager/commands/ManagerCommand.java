59044edea2acb2c6a7954155533c5ac937c06c3a
flaweoff <67743907+flaweoff@users.noreply.github.com>
2021-05-16 02:57:13 +0300

    Updated api usage

--- plugin/src/main/java/net/flawe/offlinemanager/commands/ManagerCommand.java
+++ plugin/src/main/java/net/flawe/offlinemanager/commands/ManagerCommand.java
@@ -1,9 +1,11 @@
 package net.flawe.offlinemanager.commands;
 
 import net.flawe.offlinemanager.OfflineManager;
-import net.flawe.offlinemanager.OfflineManagerAPI;
-import net.flawe.offlinemanager.api.ICommand;
-import net.flawe.offlinemanager.commands.subs.*;
+import net.flawe.offlinemanager.api.OfflineManagerAPI;
+import net.flawe.offlinemanager.api.command.ICommand;
+import net.flawe.offlinemanager.commands.sub.*;
+import net.flawe.offlinemanager.configuration.Messages;
+import net.flawe.offlinemanager.configuration.Settings;
 import net.flawe.offlinemanager.util.configuration.PlaceholderUtil;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -18,29 +20,31 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static net.flawe.offlinemanager.util.constants.Messages.*;
 import static net.flawe.offlinemanager.util.constants.Permissions.*;
+import static net.flawe.offlinemanager.util.ColorUtils.*;
 
 public class ManagerCommand implements CommandExecutor, TabCompleter {
 
     private final OfflineManagerAPI api = OfflineManager.getApi();
+    private final Messages messages = ((OfflineManager) api).getMessages();
+    private final Settings settings = ((OfflineManager) api).getSettings();
 
     public ManagerCommand() {
-        api.getCommandManager().addSubCommand(new InventoryCommand("invsee", "Open player inventory", invsee));
-        api.getCommandManager().addSubCommand(new ReloadCommand("reload", "Reload plugin configuration", reload));
+        api.getCommandManager().addSubCommand(new InventoryCommand("invsee", "Open player inventory", OFFLINEMANAGER_INVSEE));
+        api.getCommandManager().addSubCommand(new ReloadCommand("reload", "Reload plugin configuration", OFFLINEMANAGER_RELOAD));
 //        api.getCommandManager().addSubCommand(new UpdateConfigCommand("update", "Update configuration file", updateConfig));
-        api.getCommandManager().addSubCommand(new TeleportCommand("teleport", "Teleport to player", teleport, new String[]{"tp"}));
-        api.getCommandManager().addSubCommand(new CreativeCommand("creative", "Set creative mode for player", creative));
-        api.getCommandManager().addSubCommand(new SurvivalCommand("survival", "Set survival mode for player", survival));
-        api.getCommandManager().addSubCommand(new SpectatorCommand("spectator", "Set spectator mode for player", spectator));
-        api.getCommandManager().addSubCommand(new AdventureCommand("adventure", "Set adventure mode for player", adventure));
-        api.getCommandManager().addSubCommand(new ClearCommand("clear", "Clear offline player inventory", clear));
-        api.getCommandManager().addSubCommand(new TeleportHereCommand("tphere", "Teleport offline player to yourself", tphere));
-        api.getCommandManager().addSubCommand(new KillPlayerCommand("kill", "Kill offline player", kill));
-        api.getCommandManager().addSubCommand(new HealCommand("heal", "Heal offline player", heal));
-        api.getCommandManager().addSubCommand(new FeedPlayerCommand("feed", "Feed offline player", feed));
-        api.getCommandManager().addSubCommand(new EnderChestCommand("enderchest", "Open offline player enderchest", enderchest, new String[]{"ec"}));
-        api.getCommandManager().addSubCommand(new HelpCommand("help", "Get command list for plugin", usage));
+        api.getCommandManager().addSubCommand(new TeleportCommand("teleport", "Teleport to player", OFFLINEMANAGER_TELEPORT, new String[]{"tp"}));
+        api.getCommandManager().addSubCommand(new CreativeCommand("creative", "Set creative mode for player", OFFLINEMANAGER_CREATIVE));
+        api.getCommandManager().addSubCommand(new SurvivalCommand("survival", "Set survival mode for player", OFFLINEMANAGER_SURVIVAL));
+        api.getCommandManager().addSubCommand(new SpectatorCommand("spectator", "Set spectator mode for player", OFFLINEMANAGER_SPECTATOR));
+        api.getCommandManager().addSubCommand(new AdventureCommand("adventure", "Set adventure mode for player", OFFLINEMANAGER_ADVENTURE));
+        api.getCommandManager().addSubCommand(new ClearCommand("clear", "Clear offline player inventory", OFFLINEMANAGER_CLEAR));
+        api.getCommandManager().addSubCommand(new TeleportHereCommand("tphere", "Teleport offline player to yourself", OFFLINEMANAGER_TPHERE));
+        api.getCommandManager().addSubCommand(new KillPlayerCommand("kill", "Kill offline player", OFFLINEMANAGER_KILL));
+        api.getCommandManager().addSubCommand(new HealCommand("heal", "Heal offline player", OFFLINEMANAGER_HEAL));
+        api.getCommandManager().addSubCommand(new FeedPlayerCommand("feed", "Feed offline player", OFFLINEMANAGER_FEED));
+        api.getCommandManager().addSubCommand(new EnderChestCommand("enderchest", "Open offline player enderchest", OFFLINEMANAGER_ENDERCHEST, new String[]{"ec"}));
+        api.getCommandManager().addSubCommand(new HelpCommand("help", "Get command list for plugin", OFFLINEMANAGER_USAGE));
     }
 
     @Override
@@ -51,21 +55,21 @@ public class ManagerCommand implements CommandExecutor, TabCompleter {
         }
         Player p = (Player) sender;
         if (args.length == 0) {
-            p.sendMessage(api.getConfigManager().getMessageString(p, enterSubCommand)
-                    .replace("%player%", p.getName()));
+            p.sendMessage(getFormattedString(messages.getEnterSubCommand()
+                    .replace("%player%", p.getName())));
             return true;
         }
         if (!hasCommand(args[0])) {
-            p.sendMessage(api.getConfigManager().getMessageString(p, commandNotFound)
+            p.sendMessage(getFormattedString(messages.getCommandNotFound()
                     .replace("%player%", p.getName())
-                    .replace("%command%", args[0]));
+                    .replace("%command%", args[0])));
             return true;
         }
         ICommand command = getCommand(args[0]);
         if (command == null)
             return true;
         if (!sender.hasPermission(command.getPermission())) {
-            sender.sendMessage(PlaceholderUtil.fillPlaceholders(api.getConfigManager().getMessageString(p, permissionDeny), command.getPlaceholders()));
+            sender.sendMessage(PlaceholderUtil.fillPlaceholders(getFormattedString(messages.getPermissionDeny()), command.getPlaceholders()));
             return true;
         }
         command.execute(p, args);
@@ -80,7 +84,7 @@ public class ManagerCommand implements CommandExecutor, TabCompleter {
         return api.getCommandManager().getSubCommands()
                 .stream()
                 .filter(cmd -> cmd.getName().equalsIgnoreCase(name) || Arrays.stream(cmd.getAliases()).anyMatch(a -> a.equalsIgnoreCase(name)))
-                .findFirst().get();
+                .findFirst().orElse(null);
     }
 
     private boolean hasCommand(String name) {
@@ -95,12 +99,12 @@ public class ManagerCommand implements CommandExecutor, TabCompleter {
         if (!(sender instanceof Player))
             return Collections.emptyList();
         Player p = (Player) sender;
-        if (args.length == 1 && api.getConfigManager().commandComplete())
+        if (args.length == 1 && settings.isCommandComplete())
             return api.getCommandManager().getSubCommands().stream()
                     .filter(c -> p.hasPermission(c.getPermission()) && c.getName().toLowerCase().startsWith(args[0].toLowerCase()))
                     .flatMap(c -> Arrays.stream(c.getAliases()))
                     .collect(Collectors.toList());
-        if (args.length == 2 && api.getConfigManager().playerComplete())
+        if (args.length == 2 && settings.isPlayerComplete())
             return api.getStorage().getListForComplete(args);
         if (args.length == 3 && args[0].equalsIgnoreCase("invsee"))
             return Collections.singletonList("armor");
