1ce875cf625cdca5ce3787cf03e2e57aa6ae3124
flaweoff <67743907+flaweoff@users.noreply.github.com>
2021-05-16 03:14:09 +0300

    OfflineManagerAPI.java updated imports and moved to api directory

--- api/src/main/java/net/flawe/offlinemanager/api/OfflineManagerAPI.java
+++ api/src/main/java/net/flawe/offlinemanager/api/OfflineManagerAPI.java
@@ -1,6 +1,11 @@
-package net.flawe.offlinemanager;
+package net.flawe.offlinemanager.api;
 
 import net.flawe.offlinemanager.api.*;
+import net.flawe.offlinemanager.api.command.ICommandManager;
+import net.flawe.offlinemanager.api.data.IConfigManager;
+import net.flawe.offlinemanager.api.data.INMSManager;
+import net.flawe.offlinemanager.api.memory.ISession;
+import net.flawe.offlinemanager.api.memory.IStorage;
 
 import java.util.UUID;
 
@@ -14,4 +19,5 @@ public interface OfflineManagerAPI {
     ICommandManager getCommandManager();
     String getVersion();
     boolean papi();
+    void reload();
 }
