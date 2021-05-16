25c55baca2b57f45296dd1e363ed69ee9f7f720c
flaweoff <67743907+flaweoff@users.noreply.github.com>
2021-05-16 03:13:28 +0300

    OfflineManager.java Added new methods, fields and updated api usage

--- plugin/src/main/java/net/flawe/offlinemanager/OfflineManager.java
+++ plugin/src/main/java/net/flawe/offlinemanager/OfflineManager.java
@@ -1,19 +1,33 @@
 package net.flawe.offlinemanager;
 
+import net.flawe.annotation.ConfigurationLoader;
 import net.flawe.offlinemanager.api.*;
+import net.flawe.offlinemanager.api.command.ICommandManager;
+import net.flawe.offlinemanager.api.data.IConfigManager;
+import net.flawe.offlinemanager.api.data.INMSManager;
+import net.flawe.offlinemanager.api.memory.ISession;
+import net.flawe.offlinemanager.api.memory.IStorage;
 import net.flawe.offlinemanager.commands.CommandManager;
 import net.flawe.offlinemanager.commands.ManagerCommand;
+import net.flawe.offlinemanager.configuration.Messages;
+import net.flawe.offlinemanager.configuration.Settings;
 import net.flawe.offlinemanager.expansion.OfflineManagerExpansion;
+import net.flawe.offlinemanager.expansion.PAPIHelper;
 import net.flawe.offlinemanager.listeners.bukkit.InventoryListener;
 import net.flawe.offlinemanager.listeners.bukkit.PlayerListener;
 import net.flawe.offlinemanager.listeners.manager.OfflineInventoryListener;
 import net.flawe.offlinemanager.listeners.manager.OfflinePlayerListener;
-import net.flawe.offlinemanager.util.*;
+import net.flawe.offlinemanager.util.ClassAccessor;
+import net.flawe.offlinemanager.util.memory.Session;
+import net.flawe.offlinemanager.util.memory.Storage;
 import net.flawe.offlinemanager.util.configuration.ConfigManager;
 import org.bukkit.Bukkit;
 import org.bukkit.command.PluginCommand;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.UUID;
 
 public class OfflineManager extends JavaPlugin implements OfflineManagerAPI {
@@ -24,22 +38,68 @@ public class OfflineManager extends JavaPlugin implements OfflineManagerAPI {
     private IStorage storage;
     private ClassAccessor accessor;
     private ICommandManager commandManager;
+    private INMSManager nmsManager;
+    private PAPIHelper papiHelper;
+
+    private Settings settings;
+    private Messages messages;
 
     @Override
     public void onEnable() {
+        preInit();
+        init();
+        postInit();
+        info("Enabled!");
+    }
+
+    private void preInit() {
+        try {
+            Class.forName("net.flawe.offlinemanager.api.util." + getServerVersion() + ".NMSManager");
+        } catch (ClassNotFoundException e) {
+            err("This version isn't supported!");
+            Bukkit.getPluginManager().disablePlugin(this);
+            return;
+        }
         OfflineManager.api = this;
         configManager = new ConfigManager(this);
+        if (getDataFolder().mkdirs()) {
+            info("Plugin directory success created!");
+        }
+        settings = new Settings();
+        messages = new Messages(this);
+        ConfigurationLoader.loadConfiguration(settings, getConfig());
+        ConfigurationLoader.loadConfiguration(messages, YamlConfiguration.loadConfiguration(new File(getDataFolder(), "messages.yml")));
+        try {
+            ConfigurationLoader.saveConfiguration(settings, new File(getDataFolder(), "config.yml"));
+            ConfigurationLoader.saveConfiguration(messages, new File(getDataFolder(), "messages.yml"));
+        } catch (IllegalAccessException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void init() {
         storage = new Storage(this);
         session = new Session();
         accessor = new ClassAccessor(this);
-        commandManager = new CommandManager();
-        initFiles();
+        nmsManager = accessor.getNMSManager();
         storage.init();
-        initCommands();
-        initEvents();
-        if (papi() && configManager.placeholders())
+    }
+
+    private void postInit() {
+        commandManager = new CommandManager();
+        if (!settings.isOnlyApi()) {
+            initCommands();
+            initEvents();
+        }
+        if (papi())
             new OfflineManagerExpansion(this).register();
-        info("Enabled!");
+        papiHelper = new PAPIHelper(this);
+    }
+
+    @Override
+    public void reload() {
+        ConfigurationLoader.loadConfiguration(settings, getConfig());
+        ConfigurationLoader.loadConfiguration(messages, YamlConfiguration.loadConfiguration(new File(getDataFolder(), "messages.yml")));
     }
 
     @Override
@@ -47,9 +107,16 @@ public class OfflineManager extends JavaPlugin implements OfflineManagerAPI {
         info("Disabled!");
     }
 
-    private void initFiles() {
-        configManager.createConfig();
-        configManager.createMessages();
+    public PAPIHelper getPapiHelper() {
+        return papiHelper;
+    }
+
+    public Settings getSettings() {
+        return settings;
+    }
+
+    public Messages getMessages() {
+        return messages;
     }
 
     private void initEvents() {
@@ -75,12 +142,12 @@ public class OfflineManager extends JavaPlugin implements OfflineManagerAPI {
 
     @Override
     public IUser getUser(String username) {
-        return accessor.getUser(username);
+        return nmsManager.getUser(username);
     }
 
     @Override
     public IUser getUser(UUID uuid) {
-        return accessor.getUser(uuid);
+        return nmsManager.getUser(uuid);
     }
 
     @Override
@@ -90,7 +157,7 @@ public class OfflineManager extends JavaPlugin implements OfflineManagerAPI {
 
     @Override
     public INMSManager getNMSManager() {
-        return accessor.getNMSManager();
+        return nmsManager;
     }
 
     @Override
@@ -115,7 +182,7 @@ public class OfflineManager extends JavaPlugin implements OfflineManagerAPI {
 
     @Override
     public boolean papi() {
-        return Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null;
+        return Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null && settings.placeholders();
     }
 
     public void info(String s) {
