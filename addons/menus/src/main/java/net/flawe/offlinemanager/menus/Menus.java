598d4f4ce20ee59d9d3a209f0a3eddacb077f8c5
flaweoff <67743907+flaweoff@users.noreply.github.com>
2021-06-08 19:25:13 +0300

    Added future addon class

---
+++ addons/menus/src/main/java/net/flawe/offlinemanager/menus/Menus.java
@@ -0,0 +1,16 @@
+package net.flawe.offlinemanager.menus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Menus extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
