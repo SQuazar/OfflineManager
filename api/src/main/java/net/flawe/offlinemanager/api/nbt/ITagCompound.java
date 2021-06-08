d363b4a917ea338f79d6423494b39bcf504282f4
flaweoff <67743907+flaweoff@users.noreply.github.com>
2021-06-08 19:18:05 +0300

    Added tag types constants

--- api/src/main/java/net/flawe/offlinemanager/api/nbt/ITagCompound.java
+++ api/src/main/java/net/flawe/offlinemanager/api/nbt/ITagCompound.java
@@ -8,4 +8,15 @@ public interface ITagCompound {
 
     void setValue(String key, TagValue<?> value);
 
+    class TagTypes {
+        public static final byte BYTE = 1;
+        public static final byte SHORT = 2;
+        public static final byte INTEGER = 3;
+        public static final byte LONG = 4;
+        public static final byte FLOAT = 5;
+        public static final byte DOUBLE = 6;
+        public static final byte STRING = 8;
+        public static final byte LIST = 9;
+    }
+
 }
