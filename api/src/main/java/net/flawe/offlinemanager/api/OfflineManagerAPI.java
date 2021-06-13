17cc204887d56a16bbd1cda8595a2365cbf849e3
flaweoff <67743907+flaweoff@users.noreply.github.com>
2021-06-13 04:48:39 +0300

    Added javadoc

--- api/src/main/java/net/flawe/offlinemanager/api/OfflineManagerAPI.java
+++ api/src/main/java/net/flawe/offlinemanager/api/OfflineManagerAPI.java
@@ -10,31 +10,88 @@ import net.flawe.offlinemanager.api.memory.IStorage;
 
 import java.util.UUID;
 
+/**
+ * API class for interact with plugin functions
+ * @author flawe
+ */
 public interface OfflineManagerAPI {
 
+    /**
+     * Gets the offline user.
+     * @deprecated use getPlayerData method
+     * @param username target player username
+     * @return offline player instance
+     */
     @Deprecated
     IUser getUser(String username);
 
+    /**
+     * Gets the offline user.
+     * @deprecated use getPlayerData method
+     * @param uuid target player uuid
+     * @return offline player instance
+     */
     @Deprecated
     IUser getUser(UUID uuid);
 
+    /**
+     * Gets the offline player data
+     * @param username player name
+     * @return data of player
+     */
     IPlayerData getPlayerData(String username);
 
+    /**
+     * Gets the offline player data
+     * @param uuid player uuid
+     * @return data of player
+     */
     IPlayerData getPlayerData(UUID uuid);
 
+    /**
+     * Gets the plugin configuration manager
+     * @return configuration manager
+     */
     IConfigManager getConfigManager();
 
+    /**
+     * Gets class-helper for getting PlayerData and other classes whose work depend NMS
+     * @return NMSManager based on server version
+     */
     INMSManager getNMSManager();
 
+    /**
+     * Gets the plugin storage (nicknames, player data cache)
+     * @return storage instance from plugin class
+     */
     IStorage getStorage();
 
+    /**
+     * Gets the class-helper for control inventory access
+     * @return storage instance from plugin class
+     */
     ISession getSession();
 
+    /**
+     * Gets the plugin command manager. You can add your own commands
+     * @return command manager
+     */
     ICommandManager getCommandManager();
 
+    /**
+     * Gets the your server version
+     * @return server version
+     */
     String getVersion();
 
+    /**
+     * Checks for PlaceholderAPI in the plugins folder
+     * @return checks result
+     */
     boolean papi();
 
+    /**
+     * Reload the plugin
+     */
     void reload();
 }
--- api/src/main/java/net/flawe/offlinemanager/api/nbt/TagValue.java
+++ api/src/main/java/net/flawe/offlinemanager/api/nbt/TagValue.java
@@ -2,11 +2,27 @@ package net.flawe.offlinemanager.api.nbt;
 
 import net.flawe.offlinemanager.api.nbt.type.TagType;
 
+/**
+ * Tag value
+ * @param <T> tag type
+ */
 public interface TagValue<T> {
 
+    /**
+     * Gets the tag value
+     * @return value
+     */
     T getValue();
 
+    /**
+     * Set new tag value
+     * @param value new value
+     */
     void setValue(T value);
 
+    /**
+     * Gets the tag type
+     * @return tag type
+     */
     TagType getType();
 }
