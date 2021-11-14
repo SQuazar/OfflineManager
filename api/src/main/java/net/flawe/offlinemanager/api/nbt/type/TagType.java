7d45fcc7f751e1c5d484d29388c48dbfb70e0682
flaweoff <67743907+flaweoff@users.noreply.github.com>
2021-11-14 14:34:38 +0300

    Some changes

--- api/src/main/java/net/flawe/offlinemanager/api/nbt/type/TagType.java
+++ api/src/main/java/net/flawe/offlinemanager/api/nbt/type/TagType.java
@@ -1,12 +1,45 @@
+/*
+ * Copyright (c) 2021 flaweoff
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package net.flawe.offlinemanager.api.nbt.type;
 
 public enum TagType {
-    BYTE,
-    SHORT,
-    INTEGER,
-    LONG,
-    FLOAT,
-    DOUBLE,
-    STRING,
-    LIST
+    BYTE((byte) 1),
+    SHORT((byte) 2),
+    INTEGER((byte) 3),
+    LONG((byte) 4),
+    FLOAT((byte) 5),
+    DOUBLE((byte) 6),
+    STRING((byte) 8),
+    LIST((byte) 9),
+    COMPOUND((byte) 10);
+
+    private final byte type;
+
+    TagType(byte type) {
+        this.type = type;
+    }
+
+    public byte getType() {
+        return type;
+    }
 }
