e71caf1fd116d744167af247c10d5e09d55bbda4
SQuazar <flawesv@yandex.ru>
2024-03-09 21:42:02 +0300

    Added sendPlayerMessage method with Map in params

--- plugin/src/main/java/net/quazar/offlinemanager/commands/CommandHelper.java
+++ plugin/src/main/java/net/quazar/offlinemanager/commands/CommandHelper.java
@@ -6,9 +6,15 @@ import net.quazar.offlinemanager.placeholders.PlaceholderUtil;
 import org.bukkit.entity.Player;
 
 import java.util.Collection;
+import java.util.Map;
 
 public final class CommandHelper {
     public static void sendPlayerMessage(Player player, String message, Collection<IPlaceholder> placeholders) {
+        message = OfflineManager.getApi().getConfigManager().fillMessage(player, message);
+        player.sendMessage(PlaceholderUtil.fillPlaceholders(message, placeholders));
+    }
+
+    public static void sendPlayerMessage(Player player, String message, Map<String, String> placeholders) {
         message = OfflineManager.getApi().getConfigManager().fillMessage(player, message);
         player.sendMessage(PlaceholderUtil.fillPlaceholders(message, placeholders));
     }
