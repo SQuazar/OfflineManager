64eec832776938b8c2cb2f17b52bed6511b77a47
SQuazar <flawesv@yandex.ru>
2024-03-09 23:31:41 +0300

    Added new methods

--- plugin/src/main/java/net/quazar/offlinemanager/commands/CommandHelper.java
+++ plugin/src/main/java/net/quazar/offlinemanager/commands/CommandHelper.java
@@ -3,6 +3,8 @@ package net.quazar.offlinemanager.commands;
 import net.quazar.offlinemanager.OfflineManager;
 import net.quazar.offlinemanager.api.IPlaceholder;
 import net.quazar.offlinemanager.placeholders.PlaceholderUtil;
+import net.quazar.offlinemanager.util.ColorUtils;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.util.Collection;
@@ -22,5 +24,36 @@ public final class CommandHelper {
     public static void sendPlayerMessage(Player player, String message) {
         message = OfflineManager.getApi().getConfigManager().fillMessage(player, message);
         player.sendMessage(message);
+    }
+
+    public static void sendMessage(Player player, String message) {
+        message = OfflineManager.getApi().getConfigManager().fillMessage(player, message);
+        player.sendMessage(message);
+    }
+
+    public static void sendMessage(CommandSender sender, String message) {
+        if (sender instanceof Player) {
+            sendPlayerMessage((Player) sender, message);
+            return;
+        }
+        sender.sendMessage(ColorUtils.getFormattedString(message));
+    }
+
+    public static void sendMessage(CommandSender sender, String message, Map<String, String> placeholders) {
+        if (sender instanceof Player) {
+            sendPlayerMessage((Player) sender, message, placeholders);
+            return;
+        }
+        message = PlaceholderUtil.fillPlaceholders(message, placeholders);
+        sender.sendMessage(ColorUtils.getFormattedString(message));
+    }
+
+    public static void sendMessage(CommandSender sender, String message, Collection<IPlaceholder> placeholders) {
+        if (sender instanceof Player) {
+            sendPlayerMessage((Player) sender, message, placeholders);
+            return;
+        }
+        message = PlaceholderUtil.fillPlaceholders(message, placeholders);
+        sender.sendMessage(ColorUtils.getFormattedString(message));
     }
 }
