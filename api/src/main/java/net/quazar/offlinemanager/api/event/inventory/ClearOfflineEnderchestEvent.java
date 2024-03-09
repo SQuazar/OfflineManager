338a7ad83e32106758cf931444cdaa266fbb092b
SQuazar <flawesv@yandex.ru>
2024-03-10 00:30:30 +0300

    Now Clear events isn't attached to player

--- api/src/main/java/net/quazar/offlinemanager/api/event/inventory/ClearOfflineEnderchestEvent.java
+++ api/src/main/java/net/quazar/offlinemanager/api/event/inventory/ClearOfflineEnderchestEvent.java
@@ -9,28 +9,17 @@ import org.bukkit.event.Cancellable;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * Called when the player clears the offline player's enderchest
+ * Called when offline player enderchest is cleared
  * @author quazar
  */
 public class ClearOfflineEnderchestEvent extends OfflineManagerEvent implements Cancellable {
-    private final Player player;
     private final IPlayerData playerData;
     @Getter
     @Setter
     private boolean cancelled;
 
-    public ClearOfflineEnderchestEvent(@NotNull Player player, @NotNull IPlayerData playerData) {
-        this.player = player;
+    public ClearOfflineEnderchestEvent(@NotNull IPlayerData playerData) {
         this.playerData = playerData;
-    }
-
-    /**
-     * Gets the player who cleared the enderchest
-     * @return the player who cleared the enderchest
-     */
-    @NotNull
-    public Player getPlayer() {
-        return player;
     }
 
     /**
--- api/src/main/java/net/quazar/offlinemanager/api/event/inventory/ClearOfflineInventoryEvent.java
+++ api/src/main/java/net/quazar/offlinemanager/api/event/inventory/ClearOfflineInventoryEvent.java
@@ -26,32 +26,20 @@ import lombok.Getter;
 import lombok.Setter;
 import net.quazar.offlinemanager.api.data.entity.IPlayerData;
 import net.quazar.offlinemanager.api.event.OfflineManagerEvent;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * Called when the player clears the offline player's inventory
+ * Called when offline player inventory is cleared
  * @author quazar
  */
 public class ClearOfflineInventoryEvent extends OfflineManagerEvent implements Cancellable {
-    private final Player player;
     private final IPlayerData playerData;
     @Getter @Setter
     private boolean cancelled;
 
-    public ClearOfflineInventoryEvent(@NotNull Player player, @NotNull IPlayerData playerData) {
-        this.player = player;
+    public ClearOfflineInventoryEvent(@NotNull IPlayerData playerData) {
         this.playerData = playerData;
-    }
-
-    /**
-     * Gets the player who cleared the inventory
-     * @return the player who cleared the inventory
-     */
-    @NotNull
-    public Player getPlayer() {
-        return player;
     }
 
     /**
